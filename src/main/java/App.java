cee46b788a0a9ef1cd26c32f2b174dc768a28d96
beemilly <beemillyjp@gmail.com>
2020-04-11 13:40:23 +0900

    0411업데이트_두번째

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,9 +8,11 @@ public class App {
 
 	public static void main(String[] args) {
 
+		// jpa 사용에 필요한 필수객체 두개
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("testdb");
 		EntityManager em = emf.createEntityManager();
 				
+		// select * from jpa_test where no = 2;
 		Jpa_Test result = em.find(Jpa_Test.class, 2); //no
 				
 		System.out.println(result);
